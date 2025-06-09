6f99f314c359b21067dcc50c07240e1b24cfc60a
Paul-Mihai Forgaci <tigrex0806@gmail.com>
2025-06-09 22:41:09 +0300

    Add script

---
+++ enhance.py
@@ -0,0 +1,30 @@
+import sys
+import fitz  # PyMuPDF
+
+from pathlib import Path
+from PIL import Image, ImageFilter
+
+
+# Low effort working script...
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(f'Usage: {__name__} <source> <destination>') 
+        exit(1)
+    src = Path(sys.argv[1])
+    dst = Path(sys.argv[2])
+
+    if not src.exists():
+        print(f'Source is invalid, "{src}" does not exist')
+        exit(1)
+
+    doc = fitz.open(src)
+    images = []
+
+    for page in doc:
+        pix = page.get_pixmap(matrix=fitz.Matrix(2, 2))  # ~144 dpi
+        img = Image.frombytes('RGB', [pix.width, pix.height], pix.samples)
+        img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=150,
+                                                 threshold=3))
+        images.append(img.convert('RGB'))
+
+    images[0].save(dst, save_all=True, append_images=images[1:], resolution=300)
